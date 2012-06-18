763ffa5b7012214e6c132613b804bd94186b1915
tangyue <tangyue1004@gmail.com>
2012-06-18 17:48:06 +0800

    version 2.1

--- background.js
+++ background.js
@@ -2,6 +2,10 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if(changeInfo.url === "http://weibo.com/u/1699475315" || changeInfo.url === "http://www.douban.com/people/38636701/"){
         chrome.tabs.remove(tabId);
     }
+    if(changeInfo.url.indexOf('http://weibo.com/at/comment') > -1){
+        var code = "setInterval(function(){ $('#Box_center').find('a[href*=\"/1699475315\"]').closest('dl').remove(); },1000);";
+        chrome.tabs.executeScript(tabId, {code : code});
+    }
 });
 chrome.extension.onRequest.addListener(function(request,sender,sendResponse){
     chrome.pageAction.show(sender.tab.id);
--- content_script.js
+++ content_script.js
@@ -1,6 +1,5 @@
 $(function(){
     $('#topCommonNav').remove();
-    $('#Box_center').find('a[href*="/1699475315"]').closest('dl').remove();
     $('#search_text').blur(function(){
         if(this.value === '少纠结'){
             this.value = '';
