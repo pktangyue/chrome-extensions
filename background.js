be86d6486ca3a39924a52d95c3828afaedd7ba2c
tangyue <tangyue1004@gmail.com>
2012-06-18 11:27:16 +0800

    version 2.0

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+    if(changeInfo.url === "http://weibo.com/u/1699475315" || changeInfo.url === "http://www.douban.com/people/38636701/"){
+        chrome.tabs.remove(tabId);
+    }
+});
 chrome.extension.onRequest.addListener(function(request,sender,sendResponse){
     chrome.pageAction.show(sender.tab.id);
 });
--- content_script.js
+++ content_script.js
@@ -1,3 +1,10 @@
-var element = document.getElementById('topCommonNav')
-element.parentNode.removeChild(element);
-chrome.extension.sendRequest({},function(response){});
+$(function(){
+    $('#topCommonNav').remove();
+    $('#Box_center').find('a[href*="/1699475315"]').closest('dl').remove();
+    $('#search_text').blur(function(){
+        if(this.value === '少纠结'){
+            this.value = '';
+        }
+    });
+    chrome.extension.sendRequest({},function(response){});
+});
